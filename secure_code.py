9f34f7aa9373e1cded20cec7ad7c24c6fdb9a9a7
Soham Shivram Haldankar <sohamhaldankar379@gmail.com>
2026-06-27 18:40:24 +0530

    Delete secure_code.py

--- secure_code.py
+++
@@ -1,9 +0,0 @@
-import hashlib
-
-username = input("Username: ")
-password = input("Password: ")
-
-hashed_password = hashlib.sha256(password.encode()).hexdigest()
-
-print("Username:", username)
-print("Hashed Password:", hashed_password)
