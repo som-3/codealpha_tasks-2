abc3b90e4018208cef4ab4a5cca5a6a5a447892a
Soham Shivram Haldankar <sohamhaldankar379@gmail.com>
2026-06-27 18:40:54 +0530

    Delete vulnerable_code.py

--- vulnerable_code.py
+++
@@ -1,8 +0,0 @@
-
-username = input("Username: ")
-password = input("Password: ")
-
-query = "SELECT * FROM users WHERE username='" + username + "' AND password='" + password + "'"
-
-print("Executing Query:")
-print(query)
